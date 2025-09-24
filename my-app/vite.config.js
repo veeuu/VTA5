13953069d864dd8e687df1abad7695ba1ff29f7d
Vidhisha <veeekamble@gmail.com>
2025-09-24 12:56:27 +0530

    added vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // put all node_modules in a separate chunk
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 2000 // increase warning limit to 2 MB
+  }
+});
